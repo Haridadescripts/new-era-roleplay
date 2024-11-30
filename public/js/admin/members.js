7a3098096b542a34ed7aa868a104f634d82be08f
Haridadescripts <jp0889323@gmail.com>
2024-11-30 00:53:40 -0300

    Fix: Simplify project structure for Vercel deployment

--- public/js/admin/members.js
+++
@@ -1,1 +0,0 @@
-    <script src="/js/admin/members.js"></script> 
--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-function redirectToAdminLogin() {
-    window.location.href = '/admin/login';
-}
-
-function redirectToPlayerLogin() {
-    closeLoginModal();
-    // Você pode implementar o login de player depois
-    alert('Sistema de login de players em desenvolvimento!');
-} 
